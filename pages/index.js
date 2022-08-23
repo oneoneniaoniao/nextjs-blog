528d712664493374ddfecd0a1d552d41e7dac5ff
oneoneniaoniao <76186907+oneoneniaoniao@users.noreply.github.com>
2022-08-23 16:15:21 +0900

    fix

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Head from "next/head";
 
 import Link from "next/link";
 import Date from "../components/date";
-import Layout, { siteTitle } from "../components/Layout";
+import Layout, { siteTitle } from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
 
--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,7 +1,7 @@
 import Head from "next/head";
 
 import Date from '../../components/date';
-import Layout from "../../components/Layout";
+import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export async function getStaticProps({ params }) {
